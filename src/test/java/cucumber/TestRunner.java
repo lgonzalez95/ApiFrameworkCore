f729e347b549c50f65cff1867f891257365f2550
lgonzalez95 <luis0270@gmail.com>
2021-08-16 12:37:48 -0600

    Revert "Revert "Add and retrieve user""
    This reverts commit c2a74aae4cc5ef724b934e7d3c1e35227dd1177d.

--- src/test/java/cucumber/TestRunner.java
+++ src/test/java/cucumber/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         plugin = "json:target/jsonReports/cucumber-report.json",
-        glue = {"stepDefinitions"})
+        glue = {"stepsDefinitions"})
 @RunWith(Cucumber.class)
 public class TestRunner {
 }
